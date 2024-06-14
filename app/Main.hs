fdf810f2a68c8d192651a0576b9fc1faac577e21
Matt Hunzinger <matthunz2@gmail.com>
2024-06-14 03:59:23 -0400

    Create a basic prompt

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,25 @@
-module Main where
+import System.Directory (getCurrentDirectory)
+import Data.Time (getCurrentTime, formatTime, defaultTimeLocale)
+import System.Process (readProcess)
+import Control.Exception (try, SomeException)
 
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+    cwd <- getCurrentDirectory
+    time <- getCurrentTime
+    gitBranch <- getGitBranch
+    let timeStr = formatTime defaultTimeLocale "%H:%M:%S" time
+    putStrLn $ formatPrompt cwd gitBranch timeStr
+
+formatPrompt :: FilePath -> Maybe String -> String -> String
+formatPrompt cwd gitBranch timeStr =
+    "\ESC[34m" ++ cwd ++
+    maybe "" (\branch -> " \ESC[32m(" ++ branch ++ ")") gitBranch ++
+    " \ESC[33m[" ++ timeStr ++ "]\ESC[0m\n$ "
+
+getGitBranch :: IO (Maybe String)
+getGitBranch = do
+    result <- try (readProcess "git" ["rev-parse", "--abbrev-ref", "HEAD"] "") :: IO (Either SomeException String)
+    return $ case result of
+        Left _ -> Nothing
+        Right branch -> Just (init branch)
