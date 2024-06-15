4f7fa93e68382bad1f154ce4a9842df535cf0624
Matt Hunzinger <matthunz2@gmail.com>
2024-06-15 02:31:36 -0400

    Clean up

--- app/Main.hs
+++ app/Main.hs
@@ -9,33 +9,21 @@ import System.Exit (ExitCode (ExitSuccess))
 import System.FilePath (takeFileName)
 import System.Process (readProcessWithExitCode)
 
-run :: (Traversable t) => t (IO (Maybe String)) -> IO ()
-run modules = do
-  results <- mapConcurrently id modules
-  let prompt = concatMap (fromMaybe "") results
-  putStrLn prompt
-
-app :: [IO (Maybe String)]
-app =
-  currentDirectoryModule
-    ++ stringModule " "
-    ++ gitBranchModule
-
 main :: IO ()
 main = do
-  run app
+  run
+    [ currentDirectoryModule,
+      gitBranchModule
+    ]
 
-stringModule :: String -> [IO (Maybe String)]
-stringModule s = [pure (Just s)]
+run :: [IO (Maybe String)] -> IO ()
+run modules = do
+  results <- mapConcurrently id modules
+  let prompt = unwords (map (fromMaybe "") results)
+  putStrLn prompt
 
-currentDirectoryModule :: [IO (Maybe String)]
-currentDirectoryModule = [getCurrentDirectoryIO]
-
-gitBranchModule :: [IO (Maybe String)]
-gitBranchModule = [getGitBranchIO]
-
-getCurrentDirectoryIO :: IO (Maybe String)
-getCurrentDirectoryIO = do
+currentDirectoryModule :: IO (Maybe String)
+currentDirectoryModule = do
   currentDir <- getCurrentDirectory
   let lastPart = takeFileName currentDir
   return $
@@ -45,14 +33,14 @@ getCurrentDirectoryIO = do
           ++ setSGRCode [Reset]
       )
 
-getTimeIO :: IO (Maybe String)
-getTimeIO = do
+timeModule :: IO (Maybe String)
+timeModule = do
   time <- getCurrentTime
   let timeStr = formatTime defaultTimeLocale "%H:%M:%S" time
   return $ Just timeStr
 
-getGitBranchIO :: IO (Maybe String)
-getGitBranchIO = do
+gitBranchModule :: IO (Maybe String)
+gitBranchModule = do
   result <- try (readProcessWithExitCode "git" ["rev-parse", "--abbrev-ref", "HEAD"] "") :: IO (Either SomeException (ExitCode, String, String))
   return $ case result of
     Left _ -> Nothing
