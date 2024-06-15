3abbef1b8437ab933ef2798ec834e55c0ef11834
Matt Hunzinger <matthunz2@gmail.com>
2024-06-15 02:10:06 -0400

    Add colors and git branch icon

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,12 @@
+{-# LANGUAGE DeriveAnyClass #-}
+
 import Control.Concurrent.Async (mapConcurrently)
 import Control.Exception (SomeException, try)
 import Data.Maybe (fromMaybe)
 import Data.Time (defaultTimeLocale, formatTime, getCurrentTime)
+import System.Console.ANSI.Codes (Color (Blue), ColorIntensity (Vivid), ConsoleIntensity (BoldIntensity), ConsoleLayer (Foreground), SGR (Reset, SetColor, SetConsoleIntensity), setSGRCode)
 import System.Directory (getCurrentDirectory)
+import System.FilePath (takeFileName)
 import System.Process (readProcess)
 
 run :: (Traversable t) => t (IO (Maybe String)) -> IO ()
@@ -11,12 +15,35 @@ run modules = do
   let prompt = concatMap (fromMaybe "") results
   putStrLn prompt
 
+app :: [IO (Maybe String)]
+app =
+  currentDirectoryModule
+    ++ stringModule " "
+    ++ gitBranchModule
+
 main :: IO ()
 main = do
-  run [getCurrentDirectoryIO, getTimeIO, getGitBranchIO]
+  run app
+
+stringModule :: String -> [IO (Maybe String)]
+stringModule s = [pure (Just s)]
+
+currentDirectoryModule :: [IO (Maybe String)]
+currentDirectoryModule = [getCurrentDirectoryIO]
+
+gitBranchModule :: [IO (Maybe String)]
+gitBranchModule = [getGitBranchIO]
 
 getCurrentDirectoryIO :: IO (Maybe String)
-getCurrentDirectoryIO = Just <$> getCurrentDirectory
+getCurrentDirectoryIO = do
+  currentDir <- getCurrentDirectory
+  let lastPart = takeFileName currentDir
+  return $
+    Just
+      ( setSGRCode [SetColor Foreground Vivid Blue, SetConsoleIntensity BoldIntensity]
+          ++ lastPart
+          ++ setSGRCode [Reset]
+      )
 
 getTimeIO :: IO (Maybe String)
 getTimeIO = do
@@ -29,4 +56,10 @@ getGitBranchIO = do
   result <- try (readProcess "git" ["rev-parse", "--abbrev-ref", "HEAD"] "") :: IO (Either SomeException String)
   return $ case result of
     Left _ -> Nothing
-    Right branch -> Just (init branch)
+    Right branch ->
+      Just
+        ( "on "
+            ++ setSGRCode [SetColor Foreground Vivid Blue]
+            ++ "\xe725 "
+            ++ init branch
+        )
